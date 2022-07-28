e630d19fd037c85b972f16f8190d4978f32f9031
Bruno Cesar <59889935+brunoepic@users.noreply.github.com>
2022-07-28 19:09:34 -0300

    Create productAngle.js

---
+++ productAngle.js
@@ -0,0 +1,25 @@
+//CONTROLLERS ANGLES
+function imgControllerTwo(){
+    imgProduct.setAttribute('src', './images/dualsense-controller-angle.png');
+    colorcontroller.classList.add('none');
+    colorhset.classList.add('none');
+    colorcamera.classList.remove('none');
+    headTwo.classList.add('none');
+}
+
+function imgControllerThree(){
+    imgProduct.setAttribute('src', './images/dualsense-charging.png');
+    colorcontroller.classList.add('none');
+    colorhset.classList.add('none');
+    colorcamera.classList.remove('none');
+    headTwo.classList.add('none');
+}
+
+
+//HEADSETS ANGLES
+
+let headOne = document.querySelector('.colors-headset-angle1');
+let headTwo = document.querySelector('.colors-headset-angle2')
+
+let corDois = document.querySelector('.whiteh2');
+let imgColorBlackTwo = document.querySelector('.blackh2');
