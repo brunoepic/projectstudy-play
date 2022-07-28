ffa3b9ae2dd3cc50518efe829e321b9a0c115a9f
Bruno Cesar <59889935+brunoepic@users.noreply.github.com>
2022-07-28 19:10:21 -0300

    Create productColors.js

---
+++ productColors.js
@@ -0,0 +1,44 @@
+var colorWhite = document.querySelector('.white');
+
+//CONTROLLERS COLORS
+function controllerwhite(){
+    imgProduct.setAttribute('src', './images/dualsense-controller-white.png')
+}
+
+function controllerblack(){
+    imgProduct.setAttribute('src', './images/dualsense-controller-black.png')
+}
+
+function controllershock(){
+    imgProduct.setAttribute('src', './images/dualsense-controller-shock.png')
+}
+
+function controllerblue(){
+    imgProduct.setAttribute('src', './images/dualsense-controller-blue.png')
+}
+
+function controllerpink(){
+    imgProduct.setAttribute('src', './images/dualsense-controller-pink.png')
+}
+
+function controllerpurple(){
+    imgProduct.setAttribute('src', './images/dualsense-controller-purple.png')
+}
+
+
+//HEADSETS COLORS
+function imghSetWhite(){
+    imgProduct.setAttribute('src', './images/pulse-headset-white.png');
+    
+}
+
+function imghSetBlack(){
+    imgProduct.setAttribute('src', './images/pulse-headset-black.png');
+    
+}
+
+
+// COLORS FROM PRODUCTS
+let colorcontroller = document.querySelector('.colors-product-controller');
+let colorhset = document.querySelector('.colors-headset-angle1');
+let colorcamera = document.querySelector('.colors-product-camera');
